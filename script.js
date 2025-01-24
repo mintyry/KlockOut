92f6dbb0ce31002da67323c2542bc767c8b111c5
mintyry <gitmintyry@gmail.com>
2025-01-23 20:05:35 -0800

    refactored for loop to start min with 00

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // These two are node lists
 let hour = document.querySelectorAll('.select-hour');
 let min = document.querySelectorAll('.select-min');
+// array so I can access either node list in a for loop
 let inOutDropdown = [hour, min];
 
-// combine hr and min with this code block
-
+// combine hr and min with this code block; this creates dropdown options for hour and min
 for (i = 0; i < inOutDropdown.length; i++) {
+    const numStart = [1, 0];
+    const limitNum = [13, 60];
+
     inOutDropdown[i].forEach(
         function (selectEl) {
-            for (j = 1; j < 13; j++) {
+            
+            for (j = numStart[i]; j < limitNum[i]; j++) {
     
                 let option = document.createElement('option');
                 option.setAttribute('value', j);
