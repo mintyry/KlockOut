ad47ac97d6a5aeca2fbe296e12fa3bf638f46a6e
mintyry <gitmintyry@gmail.com>
2025-01-27 20:44:56 -0800

    successfully captured each select-hour value

--- script.js
+++ script.js
@@ -38,29 +38,55 @@ lunchHour.forEach(
     }
 );
 
-// access the monday IN hour and IN minute
-let monInHour = document.querySelector('#mon-in-hour');
-let monInMin = document.querySelector('#mon-in-min');
-// log values for both
-console.log(monInHour.value);
-console.log(monInMin.value);
+// // access the monday IN hour and IN minute
+// let monInHour = document.querySelector('#mon-in-hour');
+// let monInMin = document.querySelector('#mon-in-min');
+// // log values for both
+// console.log(monInHour.value);
+// console.log(monInMin.value);
 
-// function gets values of both elements,  converts hour's string to number, combines them into time, time gets updated and logged/captured
-function captureTime() {
-    let hourValue = parseInt(monInHour.value);
-    let minValue = monInMin.value;
-    let monTime = hourValue + ':' + minValue;
-    monTime = hourValue + ':' + minValue;
-    console.log(monTime);
-};
-// call function to capture default time (whatever's set at page load)
-captureTime();
+// // function gets values of both elements,  converts hour's string to number, combines them into time, time gets updated and logged/captured
+// function captureTime() {
+//     let hourValue = parseInt(monInHour.value);
+//     let minValue = monInMin.value;
+//     let monTime = hourValue + ':' + minValue;
+//     monTime = hourValue + ':' + minValue;
+//     console.log(monTime);
+// };
+// // call function to capture default time (whatever's set at page load)
+// captureTime();
 
-// then change monTime based on user's selection for IN hour and minute
-monInHour.addEventListener('change', function () {
-    captureTime();
-});
-monInMin.addEventListener('change', function () {
-    captureTime();
-});
+// // then change monTime based on user's selection for IN hour and minute
+// monInHour.addEventListener('change', function () {
+//     captureTime();
+// });
+// monInMin.addEventListener('change', function () {
+//     captureTime();
+// });
 
+// using the code above as a template, try to access and capture all in-times instead of one by one
+// use variable hour from top of script to access .select-hour
+
+// hour.value is undef, because hour is a node list
+// console.log(hour.value);
+
+// hour.forEach((dayHour) => console.log(dayHour.value));
+// min.forEach((dayMin) => console.log(dayMin.value));
+hour.forEach(
+    function(dayHour) {
+        let hourValue;
+        hourValue= parseInt(dayHour.value);
+        console.log(hourValue);
+        dayHour.addEventListener('change', function() {
+            // default value
+            hourValue= parseInt(dayHour.value);
+            console.log(hourValue);
+            // runs function again for each select-hour element, which then repeats the first console.log in this forEach function and shows the updated value
+        })
+    }
+);
+// function captureTime() {
+  
+// };
+
+// captureTime();
