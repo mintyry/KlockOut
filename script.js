d76924a93705a8a291127024ad59a9215490c9aa
mintyry <gitmintyry@gmail.com>
2025-01-29 19:26:16 -0800

    loop days elements

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let lunchHour = document.querySelectorAll('.lunch-hour');
 let hourMin = [hour, min];
 
 // access the monday IN hour and IN minute
-let monInHour = document.querySelector('#mon-in-hour');
-let monInMin = document.querySelector('#mon-in-min');
+// let monInHour = document.querySelector('#mon-in-hour');
+// let monInMin = document.querySelector('#mon-in-min');
 
 // combine hr and min with this code block; 
 for (i = 0; i < hourMin.length; i++) {
@@ -31,23 +31,36 @@ for (i = 0; i < hourMin.length; i++) {
     );
 
     // captures value for hour and minute
-    hourMin[i].forEach(
-        function (timeSelect) {
-            let timeSelectValue = parseInt(timeSelect.value);
-            // captures initial value (hr: 1)
-            console.log(timeSelectValue);
-            // event listener for when user selects an hour
-            timeSelect.addEventListener('change', function () {
-                timeSelectValue = parseInt(timeSelect.value);
-                // captures changed value
-                console.log(timeSelectValue);
-                // log values for both
-                console.log(monInHour.value);
-                console.log(monInMin.value);
-            })
-        }
-    );
+    // may not need this; favor looping days
+
+    // hourMin[i].forEach(
+    //     function (timeSelect) {
+    //         let timeSelectValue = parseInt(timeSelect.value);
+    //         // captures initial value (hr: 1)
+    //         console.log(timeSelectValue);
+    //         // event listener for when user selects an hour
+    //         timeSelect.addEventListener('change', function () {
+    //             timeSelectValue = parseInt(timeSelect.value);
+    //             // captures changed value
+    //             console.log(timeSelectValue);
+    //             // log values for both
+    //             console.log(monInHour.value);
+    //             console.log(monInMin.value);
+    //         })
+    //     }
+    // );
 };
+
+let days = [mon, tues, wed, thurs, fri];
+// loop for day id elements
+for (i = 0; i < days.length; i++) {
+    // access the monday IN hour and IN minute
+    let dayHourEl = document.querySelector(`#${days[i]}-in-hour`);
+    // let dayMinEl = document.querySelector(`#${days[i]}--in-min`);
+    console.log(dayHourEl);
+};
+
+
 
 // setting options for lunch hour; don't put a number higher than 6... because who's taking a lunch even more than 6hrs?
 lunchHour.forEach(
