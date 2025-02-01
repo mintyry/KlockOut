5d612a792551aafc1dbd8cd96c9f7b8ddfaf8d49
mintyry <gitmintyry@gmail.com>
2025-01-31 22:21:51 -0800

    successfully looped eventlistener

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ let lunchHour = document.querySelectorAll('.lunch-hour');
 // array of node lists so i can target in for loop efficiently
 let hourMin = [hour, min];
 
-// access the monday IN hour and IN minute
-// let monInHour = document.querySelector('#mon-in-hour');
-// let monInMin = document.querySelector('#mon-in-min');
-
 // combine hr and min with this code block; 
 for (i = 0; i < hourMin.length; i++) {
     const numStart = [1, 0];
@@ -29,26 +25,6 @@ for (i = 0; i < hourMin.length; i++) {
             }
         }
     );
-
-    // captures value for hour and minute
-    // may not need this; favor looping days
-
-    // hourMin[i].forEach(
-    //     function (timeSelect) {
-    //         let timeSelectValue = parseInt(timeSelect.value);
-    //         // captures initial value (hr: 1)
-    //         console.log(timeSelectValue);
-    //         // event listener for when user selects an hour
-    //         timeSelect.addEventListener('change', function () {
-    //             timeSelectValue = parseInt(timeSelect.value);
-    //             // captures changed value
-    //             console.log(timeSelectValue);
-    //             // log values for both
-    //             console.log(monInHour.value);
-    //             console.log(monInMin.value);
-    //         })
-    //     }
-    // );
 };
 
 // days array for variable and id names
@@ -58,28 +34,30 @@ let dayElements = {};
 
 // loop for day id elements
 for (i = 0; i < days.length; i++) {
-//    access elements to store in obj (index 0 is Mon, 1 is Tues, etc)
+    //    access elements to store in obj (index 0 is Mon, 1 is Tues, etc)
     dayElements[`${days[i]}InHour`] = document.querySelector(`#${days[i]}-in-hour`);
     dayElements[`${days[i]}InMin`] = document.querySelector(`#${days[i]}-in-min`);
+
     let day = days[i];
+    let hrMinString = ['InHour', 'InMin'];
+
     // logs individual hour element
-    console.log(dayElements[`${days[i]}InHour`]);
-    // logs individual element's hour value
-    console.log(dayElements[`${days[i]}InHour`].value);
+    for (j = 0; j < hrMinString.length; j++) {
+        console.log(dayElements[`${days[i] + hrMinString[j]}`]);
+        // logs individual element's hour value
+        console.log(dayElements[`${days[i] + hrMinString[j]}`].value);
 
-    dayElements[`${days[i]}InHour`].addEventListener('change', 
-        function() {
-            // declaring day variable ourside of event listener allows this codeblock to capture the correct days[i] index because  in each iteration, a new variable day is created with a unique value
-            let hourValue = parseInt(dayElements[`${day}InHour`].value);
-            console.log(day + '\'s hour: ' + hourValue);
-        }
-    )
+        let hrMin = hrMinString[j];
+
+        dayElements[`${days[i] + hrMinString[j]}`].addEventListener('change',
+            function () {
+                // declaring day variable ourside of event listener allows this codeblock to capture the correct days[i] index because  in each iteration, a new variable day is created with a unique value
+                let hrMinValue = parseInt(dayElements[`${day + hrMin}`].value);
+                console.log(day + '\'s hour or min: ' + hrMinValue);
+            }
+        )
+    }
 };
-
-
-
-
-
 
 // setting options for lunch hour; don't put a number higher than 6... because who's taking a lunch even more than 6hrs?
 lunchHour.forEach(
